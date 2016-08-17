cae1f6f8a7161c5cf08e64412e1c55b2996a9305
kwoumn3 <kwoumn@gmail.com>
2016-08-16 23:25:39 -0400

    Change sign in color to crimson

--- GreeksLink/ViewController.swift
+++ GreeksLink/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
         print(screenWidth)
         print(screenHeight)
         
-        loginButton.backgroundColor = UIColor(red: 0/255, green: 172/255, blue: 237/255, alpha: 1.0)
+        loginButton.backgroundColor = UIColor(red: 165/255, green: 14/255, blue: 44/255, alpha: 1.0)
         loginButton.layer.borderWidth = 1
         loginButton.layer.borderColor = UIColor.blackColor().CGColor
         
